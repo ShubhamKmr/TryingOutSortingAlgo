8290575ed9bbab77d34a9243c08363c8313a1da1
shubham <shubham30bksc@gmail.com>
2019-10-03 01:36:02 +0530

    Merge Sort

--- README.md
+++ README.md
@@ -12,8 +12,20 @@ Best - O(n^2)  Worst - O(n^2)  Avg - O(n^2)
 Space Complexity :
 O(1)
 
-2) Bubble Sort : - It is an inplace stable sorting algorithm in which in every ith iteration the largest element is set in its original place.
+2) Bubble Sort : - It is an inplace stable sorting algorithm in which in every ith iteration the largest element is set in its original place.We can detect whether an array is already sorted in O(n) time complexity.
 Time Complexity : 
-Best - O(n)(When the input is sorted in increasing order) Worst - O(n^2) Avg - O(n^2)
+Best - O(n)(When the input is sorted in increasing order) Worst - O(n^2) ( When array is present in decreasing order ) Avg - O(n^2)
 Space Complexity
 O(1)
+
+3) Insertion Sort : - It is an inplace stable sorting algo.Here the major idea is to select second element in the array and compare it with the all the element present in it left side.If the element in left of selected element is less.Than make a right shift of the element present of left of select element.Again comparing with left of left element and repeating the same process.Unlike in Selection and Bubble sort in One pass one element is set to its write place , in insertion sort it might or might not happen.Insertion sort has less no of swaps than bubble sort.Insertion sort performs well than other sorting algo for smalll size of array.For bigger value of array size it is inefficient.
+Time Complexity :
+Best Case : O(n) (When array is already sorted in increasing order) Worst Case : O(n^2) (When array is present in decreasing order ) Avg Case : O(n^2)
+Space Complexity :
+O(1)
+
+4)Merge Sort : - It is a outplace , stable sorting algo which uses the property of divide and conquor.It uses the concept of recursion. Here the idea is to combine the two Left and Right sorted array of size nearly equal to half of size of given unsorted array to form a sorted array.
+Time Complexity : 
+Best Case : O(nlogn) Worst Case : O(nlogn) Avg Case : O(nlogn)
+Space Complexity :
+O(n)
--- Sort.c
+++ Sort.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
 #include <limits.h>
 #define SIZE 11
+#include <stdbool.h>
 void SelectionSort(int Array[],int size);
 void BubbleSort(int Array[],int size);
+void InsertionSort(int Array[],int size);
+void MergeSort(int Array[],int size);
+void Merge(int LeftArray[] , int LeftArraySize ,int RightArray[] ,int RightArraySize, int Array[]);
 int main ()
 {
-	//int Array[SIZE] = {67,43,90,78,46,46};
-	int Array[SIZE] = {-2,0,-10,78,34,4,-56,99,34,12,-70};
-	SelectionSort(Array,SIZE);
-	BubbleSort(Array,SIZE);
+	int  i = 0;
+	int Array1[SIZE] = {-2,0,-10,78,34,4,-56,99,34,12,-70};
+	SelectionSort(Array1,SIZE);
+	int Array2[SIZE] = {1,-3,10,-3,36,8,-56,100,-1,12,8};
+	BubbleSort(Array2,SIZE);
+	int Array3[SIZE] = {-7,0,-67,7,90,-99,-5,-99,3,100,-100};
+	InsertionSort(Array3,SIZE);
+	int Array4[SIZE] = {8,0,-10,-10,34,64,-6,0,-94,2,23};
+	MergeSort(Array4,SIZE);
+	printf("\n** SORTED ARRAY OF MERGE SORT**\n");	
+	for(i = 0 ; i < SIZE ; i++)    
+		printf("%d\t",Array4[i]);
 	return 1;
 }
 
@@ -31,12 +43,13 @@ void SelectionSort(int Array[],int size)
 	}
 	printf("** SORTED ARRAY OF SELECTION SORT**\n");	
 	for(i = 0 ; i < size ; i++)    
-		printf("%d\n",Array[i]);
+		printf("%d\t",Array[i]);
 }
 
 void BubbleSort(int Array[],int size)
 {
 	int i , j , temp = 0;
+	int noSwap = true;/** To detect if the array is already sorted **/
 	for (i = 0 ; i < size ; i++)
 	{
 		for(j = 0 ; j < size-i-1 ;j++)
@@ -46,11 +59,85 @@ void BubbleSort(int Array[],int size)
 				temp = Array[j];
 				Array[j] = Array[j+1];
 				Array[j+1] = temp;
+				noSwap = false;
 			}
 		}
+		
+		if (noSwap)
+		{ 
+			printf("Array is already sorted!!");
+			break;
+		}
+		
 	}
-	printf("** SORTED ARRAY OF BUBBLE SORT**\n");	
+	printf("\n** SORTED ARRAY OF BUBBLE SORT**\n");	
 	for(i = 0 ; i < size ; i++)    
-		printf("%d\n",Array[i]);
+		printf("%d\t",Array[i]);
 
+}
+
+
+void InsertionSort(int Array[] , int size)
+{
+	int i , j ,place , key = 0;
+	for (i = 1 ; i < size ; i++)
+	{
+		key = Array[i];
+		place = i;
+		j = i-1;
+		while(key < Array[j] && j >= 0)
+		{
+
+			Array[j+1] = Array[j];
+			place = j;
+                        j--;
+		}
+		Array[place] = key;
+	}
+
+	printf("\n** SORTED ARRAY OF INSERTION SORT**\n");	
+	for(i = 0 ; i < size ; i++)    
+		printf("%d\t",Array[i]);
+}
+
+void MergeSort(int Array[],int size)
+{	
+	int i , j , mid = 0;
+	if (size < 2)return;
+	else
+	{
+		mid = size/2;
+		
+		int LArray[mid];int RArray[size - mid];
+		for (i = 0 ; i < mid ; i++)LArray[i] = Array[i];
+		for (j = mid ; j < size ;j++)RArray[j-mid] = Array[j]; 
+		MergeSort(LArray,mid);
+		MergeSort(RArray,size-mid);
+		Merge(LArray,mid,RArray,size-mid,Array);         
+	}
+}
+
+void Merge(int LSortedArray[] ,int LeftArraySize ,int RSortedArray[] ,int RightArraySize , int Array[])
+{
+	int i , j , k;
+	i = j = k = 0;
+	while (LeftArraySize > i && RightArraySize > j)
+	{
+		if (LSortedArray[i] < RSortedArray[j] )
+		{
+			Array[k++] = LSortedArray[i++];
+		}
+		else
+		{
+			Array[k++]  = RSortedArray[j++];
+		}
+	}
+	while (i != LeftArraySize)
+	{
+		Array[k++] = LSortedArray[i++];
+	}
+	while (j != RightArraySize)
+	{
+		Array[k++] = RSortedArray[j++];
+	}
 }
